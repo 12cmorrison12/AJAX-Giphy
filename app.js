a480622838960ff07d8d9987dabed36795ca879f
unknown <12cmorrison12@gmail.com>
2023-01-10 18:26:55 -0700

    working on getting axios get response to work

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //select input value and button
-let searchInput = document.getElementById('search-input').value;
-const submit = document.getElementById('submit-btn');
+const submit = document.getElementById("submit-btn");
+let searchInput = document.getElementById("search-input");
+
+async function searchGifs(e) {
+    e.preventDefault();
+    let response = await axios.get("api.giphy.com/v1/gifs/search", {params: {q: searchInput.value, api_key: "l7TS7sv9E4gKnzELrWpm4Jo3R0vNn42I"}});
+    console.log(response);
+    searchInput.value = '';
+}
 
 //add event listener to button
-submit.addEventListener("click", function (e) {
-    e.preventDefault();
-    console.log(searchInput);
-})
+submit.addEventListener("click", searchGifs);
+
