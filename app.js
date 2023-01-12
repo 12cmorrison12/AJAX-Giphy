9d6be3bb74dcd1af518c463c9f98564b3043b85f
unknown <12cmorrison12@gmail.com>
2023-01-11 17:13:46 -0700

    finished up all functions

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 //select input value and button
 const submit = document.getElementById("submit-btn");
 let searchInput = document.getElementById("search-input");
+const gifs = document.getElementById("results");
 
-async function searchGifs(e) {
-    e.preventDefault();
-    let response = await axios.get("api.giphy.com/v1/gifs/search", {params: {q: searchInput.value, api_key: "l7TS7sv9E4gKnzELrWpm4Jo3R0vNn42I"}});
-    console.log(response);
-    searchInput.value = '';
+const gifResults = (list) => {
+    let results = list.data.length;
+
+    let randomNum = Math.floor(Math.random() * results);
+    const gifDiv = document.createElement('div');
+    const newGif = document.createElement('img');
+
+    newGif.src = list.data[randomNum].images.original.url;
+
+    gifDiv.append(newGif);
+    gifs.append(gifDiv);
 }
 
 //add event listener to button
-submit.addEventListener("click", searchGifs);
+submit.addEventListener("submit", async function(e) {
+    e.preventDefault();
+    searchInput = searchInput.value;
+    const response = await axios.get("api.giphy.com/v1/gifs/search", {params: {q: searchInput, api_key: "l7TS7sv9E4gKnzELrWpm4Jo3R0vNn42I"}});
+    searchInput.value = '';
 
+    gifResults(response.data);
+});
+
+submit.addEventListener("click", function() {
+    gifDiv.empty();
+})
